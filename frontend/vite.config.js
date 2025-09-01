30378c44e9c619255acf7fe5319671890ba2c470
deevan <deevan@kali.kali>
2025-09-01 23:06:35 +0530

    first commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,12 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/multi_lan/',   
   plugins: [react()],
   optimizeDeps: {
     include: ['hoist-non-react-statics'],
     esbuildOptions: {
-
       mainFields: ['module', 'main'],
     },
   },
